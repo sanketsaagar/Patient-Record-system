393f32b507a1e81cfcdaf4b5100527423492fe2c
sanketsaagar <sanketsaagar1234@gmail.com>
2023-03-19 21:42:02 +0530

    Create Patient.sol

---
+++ Patient.sol
@@ -0,0 +1,108 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity^0.8.13;
+contract record{
+    struct patient{
+        string nameP;
+        uint idP;
+        uint age;
+        string disease;
+        address addressP;
+    }
+    struct medicine{
+        uint idM;
+        string nameM;
+        string expiryDate;
+        string dose;
+        uint price;
+    }
+    struct doctor{
+        uint idD;
+        string nameD;
+        string qualification;
+        string workplace;
+    }
+    
+    mapping(uint => patient) public Patient;
+    mapping(uint => medicine) public Medicine;
+    mapping(string => doctor) public Doctor;
+    
+    
+
+    //To register new doctor
+    function RegisterDoctor(uint _idD, string memory _nameD, string memory _qualification, string memory _workplace) public{
+        Doctor[_nameD].idD = _idD;
+        Doctor[_nameD].nameD = _nameD;
+        Doctor[_nameD].qualification = _qualification;
+        Doctor[_nameD].workplace = _workplace;
+    }
+
+    //To register a new patient
+    function registerPatient(uint _idP, string memory _nameP, uint _age, string memory _disease, address _addressP) public {
+        Patient[_idP].nameP = _nameP;
+        Patient[_idP].age = _age;
+        Patient[_idP].disease = _disease;
+        Patient[_idP].addressP = _addressP;
+    }
+
+    //Add patients disease
+    function addNewDisease(uint _idP, string memory _disease) public{
+        Patient[_idP].disease = _disease;
+    }
+
+    //Add medicine
+    function addMedicine(uint _idM, string memory _nameM, string memory _expryDate, string memory _dose, uint _price) public{
+        Medicine[_idM].nameM = _nameM;
+        Medicine[_idM].expiryDate = _expryDate;
+        Medicine[_idM].dose = _dose;
+        Medicine[_idM].price = _price;
+    }
+
+    mapping(uint => medicine[]) public prescribedMed;
+    //Add prescribed Medicine
+    function prescribedMedicine(uint _idM, address _addressP) public{
+        Medicine[_idM].idM = _idM;
+        Patient[_idM].addressP = _addressP;
+    }
+
+    //Update the age
+    function updateAge(uint _age) public{
+        Patient[_age].age = _age;
+    }
+
+    //View patient record
+    function viewRecord(uint _idP) public view returns(string memory _nameP, uint _age , string memory _disease, address _addressP){
+        return (Patient[_idP].nameP,
+        Patient[_idP].age,
+        Patient[_idP].disease,
+        Patient[_idP].addressP);
+    }
+
+    //view Medicine record
+    function viewMedicine(uint _idM) public view returns(string memory _nameM, string memory _expryDate, string memory _dose, uint _price){
+        return(Medicine[_idM].nameM,
+        Medicine[_idM].expiryDate,
+        Medicine[_idM].dose,
+        Medicine[_idM].price);
+    }
+
+    //View patient record by doctor
+    function viewRecordbyd(uint _idP) public view returns(string memory _nameP, uint _age , string memory _disease, address _addressP){
+        return (Patient[_idP].nameP,
+        Patient[_idP].age,
+        Patient[_idP].disease,
+        Patient[_idP].addressP);
+    }
+
+    //view Prescribed Medicine
+    function viewPrescribeMed(address _addressP) public view returns(uint _idM){
+        return(Medicine[_idM].idM);
+    }
+
+    
+    //View Doctor details
+    function viewDoc(uint _idD) public view returns(string memory _nameD, string memory _qualification, string memory _workplace){
+        return (Doctor[_idD].nameD = _nameD,
+        Doctor[_idD].qualification = _qualification,
+        Doctor[_idD].workplace = _workplace);
+    }
+}
